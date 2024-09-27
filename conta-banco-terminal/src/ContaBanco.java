07dafc0f7f30c887aa7161feedb06954796b1143
Willder <willderacioly20@gmail.com>
2024-09-27 14:09:05 -0300

    final conta banco terminal

--- conta-banco-terminal/src/ContaBanco.java
+++ conta-banco-terminal/src/ContaBanco.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaBanco {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+    
+        System.out.print("Por Favor informe o número da sua conta: ");
+        int numeroConta = scanner.nextInt(); //lê numero da conta
+
+        System.out.print("Por Favor informe a sua agência: ");
+        scanner.nextLine();
+        String agencia = scanner.nextLine(); //lê a agência
+
+        System.out.print("Por Favor informe seu nome: ");
+        String nomeCliente = scanner.nextLine(); //lê nome do cliente 
+
+        System.out.print("Por favor, digite o saldo:");
+        double saldoConta = scanner.nextDouble(); //lê saldo
+
+        //output 
+        System.out.println("Olá ".concat(nomeCliente) +
+        " obrigado por criar uma conta em nosso banco, " + 
+        "sua agência é ".concat(agencia) +
+        ", conta " + (numeroConta) + " e seu saldo " + (saldoConta) + 
+        " já está disponível para saque" );
+        
+        scanner.close();
     }
+   
 }
